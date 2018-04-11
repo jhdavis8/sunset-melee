09ce2e122b67cd8c65b998e70a359fbdcbd99d64
Mark Wolgin <wolginmark@gmail.com>
2018-04-10 21:48:25 -0400

    Initial setup of Git workspace and Eclipse IDE

---
+++ src/gameFiles/Controller.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gameFiles;
+
+/**
+ * @author Mark Wolgin
+ * @author Josh Davis
+ *
+ */
+public class Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/gameFiles/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Mark
+ *
+ */
+package gameFiles;
