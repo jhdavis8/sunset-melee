9adb2687eeb0be519ee15b23dd7cd4691c119074
Mark Wolgin <wolginmark@gmail.com>
2018-04-11 13:28:09 -0400

    More work on Board

--- src/gameFiles/Board.java
+++ src/gameFiles/Board.java
@@ -19,7 +19,34 @@ public class Board {
 	private int defcon;
 	private int USMilitaryOps;
 	private int USSRMilitaryOps;
+	private boolean playerTurn;
 	
+	public Board() {
+		deck = new ArrayList<Card>();
+		discard = new ArrayList<Card>();
+		turn = 0;
+		actionRound = 0;
+		victoryPoints = 0;
+		defcon = 0;
+		USMilitaryOps = 0;
+		USSRMilitaryOps = 0;
+		playerTurn = false;
+	}
+	
+	public void setUp() {
+		deck = fillDeck();
+		defcon = 5;
+		// More code to follow....
+	}
+	
+	/**
+	 * @return The complete deck as read from the .csv
+	 */
+	private ArrayList<Card> fillDeck() {
+		
+		return null;
+	}
+
 	public void placeInfluence(Side side, Country cont, int value) {
 		
 	}
@@ -36,7 +63,12 @@ public class Board {
 		
 		String toReturn = "";
 		toReturn += "TURN:" + turn + ", ACTION ROUND:" + actionRound + "\n";
-		toReturn += "";
+		toReturn += "REQUIRED MILITARY\nOPS[" + USMilitaryOps + ", " + USSRMilitaryOps + "]";
+		toReturn += "VICTORY POINTS:" + victoryPoints + "\n";
+		toReturn += "DEFCON:" + defcon + "\n";
+		toReturn += "CURRENT PLAYER:";
+		if (!playerTurn) toReturn += "USSR\n";
+		else toReturn += "USA\n";
 		
 		return toReturn;
 	}
