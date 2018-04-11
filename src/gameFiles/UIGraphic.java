220dd5609bf962e397b8be9bbb1973a40faf09a2
Mark Wolgin <wolginmark@gmail.com>
2018-04-11 12:37:29 -0400

    Final of the created classes

---
+++ src/gameFiles/UIGraphic.java
@@ -0,0 +1,23 @@
+package gameFiles;
+
+public class UIGraphic implements UICore {
+
+	@Override
+	public Board currentBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBoard(Board b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object updateUI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/gameFiles/UIText.java
@@ -0,0 +1,26 @@
+package gameFiles;
+
+public class UIText implements UICore {
+	
+
+	@Override
+	public Board currentBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBoard(Board b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String updateUI() {
+		String toReturn = "";
+		
+		
+		return toReturn;
+	}
+
+}
