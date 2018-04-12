9b07089d75cbcfeaa3a9cb68c5da6fbe81529a53
Mark Wolgin <wolginmark@gmail.com>
2018-04-12 11:02:39 -0400

    Removed "Hello, Josh"

--- src/gameFiles/Controller.java
+++ src/gameFiles/Controller.java
@@ -16,9 +16,6 @@ public class Controller {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Josh");
-		
 		Board game = new Board();
 		game.setUp();
 		UIText UI = new UIText();
