7d582f943ecae127c13908e3e5599ec1eda4c9b9
Mark Wolgin <wolginmark@gmail.com>
2018-05-03 11:35:12 -0400

    Where art though locations?

---
+++ src/gameFiles/Location.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package gameFiles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Mark
+ *
+ */
+public class Location {
+	
+	private int x;
+	private int y;
+	private String name;
+	
+	public Location (int xPos, int yPos, String nameOfLoc) {
+		x = xPos;
+		y = yPos;
+		name = nameOfLoc;
+	}
+	
+	private static ArrayList<Location> loc = new ArrayList<Location>();
+	
+	private static void fillGameLoc(File f) {
+		Scanner scan = null;
+		try {
+			scan = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		scan.useDelimiter("[,|\n]");
+	}
+
+}
