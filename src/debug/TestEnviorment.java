3a8ea998afe3d90d7d721a11993de221f69818d3
Josh Davis <jhdavis@udel.edu>
2018-05-09 13:11:17 -0400

    Hey something works

--- src/debug/TestEnviorment.java
+++ src/debug/TestEnviorment.java
@@ -3,6 +3,8 @@ package debug;
 import java.io.File;
 
 import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import gameFiles.Country;
 import gameFiles.Map;
@@ -13,6 +15,12 @@ public class TestEnviorment {
 		try {
 			Map.setFile(new File(new File("").getAbsolutePath() + "\\csv\\countries.csv"));
 			Map.fillCountries();
+			JFrame frame = new JFrame();
+			Object[] options = { "OK", "CANCEL" };
+			JOptionPane.showOptionDialog(frame, "Click OK to continue", "Warning",
+			             JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+			             null, options, options[0]);
+			frame.setVisible(true);
 			FrameTest dialog = new FrameTest();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
